5a768a1135519e8debed880a87405d9a5e9a25cd
Manu Jayaraj <manujayarajkm@gmail.com>
2024-03-10 15:47:48 +0530

    add auth guard

--- src/movie/movie-controller/movie-controller.controller.ts
+++ src/movie/movie-controller/movie-controller.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { MovieService } from '../movie-service/movie-service.service';
 import { CreateMovieDto } from '../dto/create-movie-dto';
+import { AuthGuard } from '../../auth/auth.guard';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly movieService: MovieService) {}
 
+  @UseGuards(AuthGuard)
   @Post()
   async create(@Body() createMovieDto: CreateMovieDto) {
     await this.movieService.create(createMovieDto);
@@ -16,11 +18,13 @@ export class MoviesController {
     return this.movieService.findAll();
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id')
   async update(@Param('id') id: string,@Body() updateMovieBody: CreateMovieDto){
     return this.movieService.update(id,updateMovieBody);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async delete(@Param('id') id: string) {
     return this.movieService.delete(id);
